dbcc102dfa4f26fbcbd5bff3bcb05c07604f2f43
earamosb8 <earamosb8@gmail.com>
2026-04-15 16:17:13 -0500

    fix styles css

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -9,34 +9,59 @@ import Swal from 'sweetalert2';
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
-  styleUrl: './main.component.css'
+  styleUrl: './main.component.css',
 })
-
-
 export class MainComponent {
-  nameField = new FormControl({ value: '', disabled: false }, [Validators.required, this.onlyLettersAndSpacesValidator()]);
-  identificacionField = new FormControl({ value: '', disabled: false }, [Validators.required, Validators.pattern("[0-9]+")]);
-  identificacionSearch = new FormControl({ value: '', disabled: false }, [Validators.required, Validators.pattern("[0-9]+")]);
-  celField = new FormControl({ value: '', disabled: false }, [Validators.required, Validators.pattern("[0-9]+")]);
-  dateField = new FormControl('',Validators.required);
-  nameFieldEdit = new FormControl({ value: '', disabled: false }, [Validators.required, this.onlyLettersAndSpacesValidator()]);
-  identificacionFieldEdit = new FormControl({ value: '', disabled: false }, [Validators.required, Validators.pattern("[0-9]+")]);
-  celFieldEdit = new FormControl({ value: '', disabled: false }, [Validators.required, Validators.pattern("[0-9]+")]);
-  dateFieldEdit = new FormControl('',Validators.required);
-  nombre:string='';
-  identificacion:string='';
-  celular:string='';
+  nameField = new FormControl({ value: '', disabled: false }, [
+    Validators.required,
+    this.onlyLettersAndSpacesValidator(),
+  ]);
+  identificacionField = new FormControl({ value: '', disabled: false }, [
+    Validators.required,
+    Validators.pattern('[0-9]+'),
+  ]);
+  identificacionSearch = new FormControl({ value: '', disabled: false }, [
+    Validators.required,
+    Validators.pattern('[0-9]+'),
+  ]);
+  celField = new FormControl({ value: '', disabled: false }, [
+    Validators.required,
+    Validators.pattern('[0-9]+'),
+  ]);
+  dateField = new FormControl('', Validators.required);
+  nameFieldEdit = new FormControl({ value: '', disabled: false }, [
+    Validators.required,
+    this.onlyLettersAndSpacesValidator(),
+  ]);
+  identificacionFieldEdit = new FormControl({ value: '', disabled: false }, [
+    Validators.required,
+    Validators.pattern('[0-9]+'),
+  ]);
+  celFieldEdit = new FormControl({ value: '', disabled: false }, [
+    Validators.required,
+    Validators.pattern('[0-9]+'),
+  ]);
+  dateFieldEdit = new FormControl('', Validators.required);
+  nombre: string = '';
+  identificacion: string = '';
+  celular: string = '';
   fecha = new Date();
-  usuarios:Usuarios[] =[] ;
-  usuariosFiltrados:Usuarios[] =[] ;
-  openModal:boolean=false;
-  indice: number = 0 ;
+  usuarios: Usuarios[] = [];
+  usuariosFiltrados: Usuarios[] = [];
+  openModal: boolean = false;
+  indice: number = 0;
   fechaActual: Date = new Date();
 
-  displayedColumns: string[] = ['Nombres', 'Nro.identificación', 'Celular', 'Fecha','Opciones'];
+  displayedColumns: string[] = [
+    'Nombres',
+    'Nro.identificación',
+    'Celular',
+    'Fecha',
+    'Opciones',
+  ];
   dataSource = this.usuarios;
 
-/*
+  /*
   formDatosBasicos = new FormGroup({
 
     identificacionField: new FormControl({ value: '', disabled: false }, [Validators.required, Validators.pattern("[0-9]+")]),
@@ -44,61 +69,63 @@ export class MainComponent {
     nombreField: new FormControl('', Validators.required)
   });
 */
-constructor(private datePipe: DatePipe,private cdr: ChangeDetectorRef){
+  constructor(
+    private datePipe: DatePipe,
+    private cdr: ChangeDetectorRef,
+  ) {}
 
-}
-
-  ngOnInit(){
+  ngOnInit() {
     this.fechaActual = new Date();
     console.log(this.fechaActual);
     this.cargarUsuarios();
   }
 
-
-
-
-  public  guardarUsuario():void{
-    if(this.nameField.valid && this.identificacionField.valid && this.celField.valid && this.dateField.valid){
-      let existe = this.usuarios.find(usuario => usuario.identificacion === this.identificacionField.value);
-      if(existe === undefined){
-        let usuario:Usuarios = {
-          nombre:this.nameField.value,
-          identificacion : this.identificacionField.value,
-          celular : this.celField.value ,
-          fecha: this.dateField.value
+  public guardarUsuario(): void {
+    if (
+      this.nameField.valid &&
+      this.identificacionField.valid &&
+      this.celField.valid &&
+      this.dateField.valid
+    ) {
+      let existe = this.usuarios.find(
+        (usuario) => usuario.identificacion === this.identificacionField.value,
+      );
+      if (existe === undefined) {
+        let usuario: Usuarios = {
+          nombre: this.nameField.value,
+          identificacion: this.identificacionField.value,
+          celular: this.celField.value,
+          fecha: this.dateField.value,
         };
-        if(localStorage.getItem('Usuarios')){
+        if (localStorage.getItem('Usuarios')) {
           let storedData = localStorage.getItem('Usuarios') || '{}';
           this.usuarios = JSON.parse(storedData);
           this.usuarios.push(usuario);
-          localStorage.setItem('Usuarios', JSON.stringify(this.usuarios))
-
+          localStorage.setItem('Usuarios', JSON.stringify(this.usuarios));
 
           //this.usuarios = JSON.parse(localStorage.getItem('Usuarios'));
-           /*
+          /*
             this.usuario.push(usuario);
             localStorage.setItem('Usuarios', JSON.stringify(this.usuarios))*/
-        }
-        else {
+        } else {
           this.usuarios.push(usuario);
-          localStorage.setItem('Usuarios', JSON.stringify(this.usuarios))
+          localStorage.setItem('Usuarios', JSON.stringify(this.usuarios));
         }
         this.cargarUsuarios();
-
-      }
-      else{
+      } else {
         console.log('usuario Existe');
-        Swal.fire(
-          {
-            text:"El usuario con numero de identificación " + this.identificacionField.value + " ya existe",
-            showConfirmButton: true,
-            confirmButtonText: "Ok",
-            confirmButtonColor: "#F44336"
-          }
-        )
+        Swal.fire({
+          text:
+            'El usuario con numero de identificación ' +
+            this.identificacionField.value +
+            ' ya existe',
+          showConfirmButton: true,
+          confirmButtonText: 'Ok',
+          confirmButtonColor: '#F44336',
+        });
       }
 
-/*
+      /*
  Swal.fire({
       title: "Confirmar",
       icon: "question",
@@ -111,32 +138,27 @@ constructor(private datePipe: DatePipe,private cdr: ChangeDetectorRef){
     }
 
 */
-
-    }
-
-  }
-
-
-
-    public cargarUsuarios():void{
-      if(localStorage.getItem('Usuarios')){
-        this.usuarios = [];
-        let storedData = localStorage.getItem('Usuarios') || '{}';
-        this.usuarios = JSON.parse(storedData,this.reviver);
-        this.dataSource = this.usuarios;
-        this.cdr.detectChanges();
     }
   }
 
+  public cargarUsuarios(): void {
+    if (localStorage.getItem('Usuarios')) {
+      this.usuarios = [];
+      let storedData = localStorage.getItem('Usuarios') || '{}';
+      this.usuarios = JSON.parse(storedData, this.reviver);
+      this.dataSource = this.usuarios;
+      this.cdr.detectChanges();
+    }
+  }
 
-  public reviver = (key:any, value:any) => {
+  public reviver = (key: any, value: any) => {
     if (key === 'fecha') {
       return new Date(value);
     }
     return value;
   };
 
-
+  /*
   public eliminarUsuario(user:any):void{
     let indice = this.usuarios.indexOf(user);
     this.usuarios.splice(indice,1);
@@ -145,53 +167,79 @@ constructor(private datePipe: DatePipe,private cdr: ChangeDetectorRef){
     let storedData = localStorage.getItem('Usuarios') || '{}';
     this.usuarios = JSON.parse(storedData);
     this.dataSource = this.usuarios;
+  }*/
+
+  public eliminarUsuario(user: any): void {
+    let indice = this.usuarios.indexOf(user);
+    this.usuarios.splice(indice, 1);
+    localStorage.setItem('Usuarios', JSON.stringify(this.usuarios));
+
+    let storedData = localStorage.getItem('Usuarios') || '[]';
+    this.usuarios = JSON.parse(storedData).map((u: any) => ({
+      ...u,
+      fecha: new Date(u.fecha),
+    }));
+
+    this.dataSource = this.usuarios;
   }
 
-    public editarUsuario(user:any):void{
-      this.openModal = true;
-      this.indice = this.usuarios.indexOf(user);
-      console.log(this.usuarios[this.indice].nombre);
-      this.nameFieldEdit.setValue(this.usuarios[this.indice].nombre);
-      this.identificacionFieldEdit.setValue(this.usuarios[this.indice].identificacion);
-      this.celFieldEdit.setValue(this.usuarios[this.indice].celular);
-      this.dateFieldEdit.setValue(this.usuarios[this.indice].fecha);
-    }
+  public editarUsuario(user: any): void {
+    this.openModal = true;
+    this.indice = this.usuarios.indexOf(user);
+    console.log(this.usuarios[this.indice].nombre);
+    this.nameFieldEdit.setValue(this.usuarios[this.indice].nombre);
+    this.identificacionFieldEdit.setValue(
+      this.usuarios[this.indice].identificacion,
+    );
+    this.celFieldEdit.setValue(this.usuarios[this.indice].celular);
+    this.dateFieldEdit.setValue(this.usuarios[this.indice].fecha);
+  }
 
-    public editarUsuarioSeleccionado(){
-      if(this.nameFieldEdit.valid && this.identificacionFieldEdit.valid && this.celFieldEdit.valid && this.dateFieldEdit.valid){
-        this.usuarios[this.indice].nombre = this.nameFieldEdit.value;
-        this.usuarios[this.indice].identificacion = this.identificacionFieldEdit.value;
-        this.usuarios[this.indice].celular = this.celFieldEdit.value;
-        this.usuarios[this.indice].fecha = this.dateFieldEdit.value;
-        localStorage.setItem('Usuarios', JSON.stringify(this.usuarios))
-        this.openModal= false;
+  public editarUsuarioSeleccionado() {
+    if (
+      this.nameFieldEdit.valid &&
+      this.identificacionFieldEdit.valid &&
+      this.celFieldEdit.valid &&
+      this.dateFieldEdit.valid
+    ) {
+      this.usuarios[this.indice].nombre = this.nameFieldEdit.value;
+      this.usuarios[this.indice].identificacion =
+        this.identificacionFieldEdit.value;
+      this.usuarios[this.indice].celular = this.celFieldEdit.value;
+      this.usuarios[this.indice].fecha = this.dateFieldEdit.value;
+      localStorage.setItem('Usuarios', JSON.stringify(this.usuarios));
+      this.openModal = false;
+    }
+  }
+
+  onlyLettersAndSpacesValidator() {
+    return (control: any) => {
+      if (control.value && !/^[A-Za-z\s]+$/.test(control.value)) {
+        return { invalidCharacters: true };
       }
-    }
+      return null;
+    };
+  }
 
-    onlyLettersAndSpacesValidator() {
-      return (control:any) => {
-        if (control.value && !/^[A-Za-z\s]+$/.test(control.value)) {
-          return { 'invalidCharacters': true };
-        }
-        return null;
-      };
-    }
+  public buscarUsuario(): void {
+     const busqueda = this.identificacionSearch.value?.toString().trim() ?? '';
+     if (busqueda === '') {
+       this.dataSource = this.usuarios;
+       return;
+     }
+     this.usuariosFiltrados = this.usuarios.filter((usuario) =>
+       usuario.identificacion?.toString().startsWith(busqueda),
+     );
+     this.dataSource = this.usuariosFiltrados;
 
-    public buscarUsuario():void{
-      console.log(this.identificacionSearch.value);
-      this.usuariosFiltrados = this.usuarios.filter(usuario => usuario.identificacion === this.identificacionSearch.value);
-      console.log(this.usuariosFiltrados);
-      if(this.usuariosFiltrados.length > 0){
-        this.dataSource = this.usuariosFiltrados;
-      } else {
-        this.dataSource = this.usuarios;
-      }
-    }
+  }
 
-    public validarIdExiste():void{
-      let existe = this.usuarios.find(usuario => usuario.identificacion === this.identificacionField.value);
-      console.log(existe);
-    }
+  public validarIdExiste(): void {
+    let existe = this.usuarios.find(
+      (usuario) => usuario.identificacion === this.identificacionField.value,
+    );
+    console.log(existe);
+  }
 }
 
 
